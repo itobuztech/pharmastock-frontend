5d40b462b2a1419e8cc0f627b665f88c8cc1c069
Sudeep Chowdhury <sudeep@itobuz.com>
2024-07-01 14:06:38 +0530

    FEAT: IOM-49 use state hook modified and value code modified.

--- client/src/Page/Profile/ProfilePage.tsx
+++ client/src/Page/Profile/ProfilePage.tsx
@@ -6,9 +6,19 @@ import { GET_USER } from "./profileQuery";
 import { Adminprofile } from "../../interfaces/interfaces";
 
 export default function ProfilePage() {
-  const [admin, setAdmin] = useState<Adminprofile | null>(null);
+  const [admin, setAdmin] = useState<Adminprofile>({
+    account: {
+      role: "",
+      user: {
+        email: "",
+        id: "",
+        name: "",
+        username: "",
+      },
+    },
+  });
 
-  const [getCurrentUser, { loading, data, error }] = useLazyQuery(GET_USER, {
+  const [getCurrentUser, { loading, error }] = useLazyQuery(GET_USER, {
     onCompleted: (d) => {
       if (d.account) {
         const profile: Adminprofile = {
@@ -62,7 +72,7 @@ export default function ProfilePage() {
                 className="block relative border-0"
               ></button>
               <h1 className="text-gray-600">
-                {data?.account?.user?.name ? data.account.user.name : "Name"}
+                {admin?.account?.user?.name ? admin.account.user.name : "Name"}
               </h1>
             </div>
           </div>
@@ -77,7 +87,7 @@ export default function ProfilePage() {
                   id="user-info-email"
                   className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                   placeholder="Email"
-                  value={data?.account?.user?.email}
+                  value={admin?.account?.user?.email}
                   onChange={handleChange("email")}
                 />
               </div>
@@ -94,7 +104,7 @@ export default function ProfilePage() {
                     id="user-info-name"
                     className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                     placeholder="Name"
-                    value={data?.account?.user?.username}
+                    value={admin?.account?.user?.username}
                     onChange={handleChange("username")}
                   />
                 </div>
@@ -106,7 +116,7 @@ export default function ProfilePage() {
                     id="user-info-phone"
                     className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                     placeholder="Name"
-                    value={data?.account?.user?.name}
+                    value={admin?.account?.user?.name}
                     onChange={handleChange("name")}
                   />
                 </div>
@@ -117,24 +127,35 @@ export default function ProfilePage() {
           <div className="items-center w-full p-8 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
             <h2 className="max-w-sm mx-auto md:w-4/12">Change password</h2>
             <div className="w-full max-w-sm pl-2 mx-auto space-y-5 md:w-5/12 md:pl-9 md:inline-flex">
-              <div className=" relative ">
-                <input
-                  type="text"
-                  id="user-info-password"
-                  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  placeholder="Password"
-                />
+              <div className="w-full space-y-4">
+                <div className="relative">
+                  <input
+                    type="text"
+                    id="current-password"
+                    className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    placeholder="Current Password"
+                  />
+                </div>
+                <div className="relative">
+                  <input
+                    type="text"
+                    id="new-password"
+                    className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    placeholder="New Password"
+                  />
+                </div>
               </div>
             </div>
             <div className="text-center md:w-3/12 md:pl-6">
               <button
                 type="button"
-                className="py-2 px-4  bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                className="py-2 px-4 bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
               >
                 Change
               </button>
             </div>
           </div>
+
           <hr />
           <div className="w-full px-4 pb-4 ml-auto text-gray-500 md:w-1/3">
             <button
