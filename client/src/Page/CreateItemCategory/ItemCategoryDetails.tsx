1f715aa97d883ad1a8a10a0faa31b8b0eda59fc9
Kunal Biswas <kunal@itobuz.com>
2024-07-16 16:03:59 +0530

    feat: IOM-77 code refactor done

--- client/src/Page/CreateItemCategory/ItemCategoryDetails.tsx
+++ client/src/Page/CreateItemCategory/ItemCategoryDetails.tsx
@@ -1,63 +1,22 @@
-import { Button, TextInput } from "@mantine/core";
+import React, { useState } from "react";
 import PageHeader from "Components/PageHeader";
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
-import ButtonComponent from "Components/Button/ButtonComponent";
-import { useMutation, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 import { GetCategoryItem } from "query/category/categoryItem";
-import { ItemCategory, UpdateItemCategoryInput } from "gql/graphql";
-import { ItemCategoryUpdate } from "query/category/categoryUpdate";
-import { toast } from "react-toastify";
+import { ItemCategory } from "gql/graphql";
+import ItemCategoryForm from "./components/ItemCategoryForm";
 
 export default function ItemCategoryDetails() {
   const [editForm, setEditForm] = useState(false);
-  const navigate = useNavigate();
   const { id } = useParams();
 
-  const schema = yup
-    .object({
-      name: yup.string().required(),
-    })
-    .required();
-
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
+  const { data: categoryItem, refetch } = useQuery<{
+    itemCategory: ItemCategory;
+  }>(GetCategoryItem, {
+    variables: {
+      itemCategoryId: id,
+    },
   });
-
-  const { data: categoryItem } = useQuery<{ itemCategory: ItemCategory }>(
-    GetCategoryItem,
-    {
-      variables: {
-        itemCategoryId: id,
-      },
-    }
-  );
-  const [updateItemCategory, { loading }] = useMutation(ItemCategoryUpdate);
-
-  const onSubmit = async (data: UpdateItemCategoryInput) => {
-    try {
-      await updateItemCategory({
-        variables: { updateItemCategoryInput: { ...data, id: id } },
-      });
-      toast.success("Category Updated Successfully");
-    } catch (error: any) {
-      toast.error(error.message);
-    }
-  };
-
-  useEffect(() => {
-    if (categoryItem) {
-      setValue("name", categoryItem.itemCategory.name);
-    }
-  }, [categoryItem, setValue]);
 
   return (
     <section className="min-h-screen bg-blue-50 bg-opacity-50 py-8 px-8">
@@ -67,41 +26,14 @@ export default function ItemCategoryDetails() {
         showCreateButton={false}
       />
 
-      <div className="w-1/2 bg-white rounded-md py-6 px-6">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="mb-4">
-            <TextInput
-              label="Name"
-              placeholder="Name"
-              {...register("name")}
-              readOnly={editForm ? false : true}
-            />
-            {errors.name && (
-              <span className="text-red-500 mt-2 block text-xs">
-                This field is required
-              </span>
-            )}
-          </div>
-          <div className="flex flex-wrap gap-4 justify-end mb-6 mt-8">
-            <Button
-              type="button"
-              onClick={() => navigate(-1)}
-              variant="outline"
-            >
-              Cancel
-            </Button>
-
-            {editForm ? (
-              <ButtonComponent type="submit" loading={loading}>
-                Update
-              </ButtonComponent>
-            ) : (
-              <Button type="button" onClick={() => setEditForm(true)}>
-                Edit
-              </Button>
-            )}
-          </div>
-        </form>
+      <div className="w-full lg:w-1/2 bg-white rounded-md py-6 px-6">
+        <ItemCategoryForm
+          editForm={editForm}
+          setEditForm={setEditForm}
+          catId={id}
+          refetchItemCategory={refetch}
+          categoryItem={categoryItem}
+        />
       </div>
     </section>
   );
---
+++ client/src/Page/CreateItemCategory/components/ItemCategoryTable.tsx
@@ -0,0 +1,77 @@
+import { Flex, Pagination, Space, Table } from "@mantine/core";
+import ActionPopover from "Components/ActionPopover";
+import { ItemCategories } from "interfaces/interfaces";
+import routes from "Lib/Routes/Routes";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function ItemCategoryTable({
+  activePage,
+  setActivePage,
+  itemCategoryList,
+  handleDelete,
+  totalCount,
+}: {
+  activePage: number;
+  setActivePage: React.Dispatch<React.SetStateAction<number>>;
+  itemCategoryList?: ItemCategories;
+  handleDelete: (id: string) => void;
+  totalCount: number;
+}) {
+  const navigate = useNavigate();
+
+  function screenSwitch(id: string) {
+    navigate(`${routes.dashboard.createItemCategory.path}/${id}`);
+  }
+
+  const rows = itemCategoryList?.itemCategories.map((item, i) => (
+    <Table.Tr key={item.id}>
+      <Table.Td>
+        {activePage === 1 ? i + 1 : (activePage - 1) * 10 + (i + 1)}
+      </Table.Td>
+      <Table.Td>{item.name}</Table.Td>
+      <Table.Td>12</Table.Td>
+      <Table.Td>23</Table.Td>
+      <Table.Td>
+        <ActionPopover
+          handleView={() => screenSwitch(item.id)}
+          handleDelete={() => handleDelete(item.id)}
+        />
+      </Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <div className="bg-white overflow-auto">
+      <Table horizontalSpacing="md" verticalSpacing="md">
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Sl No.</Table.Th>
+            <Table.Th>Name</Table.Th>
+            <Table.Th>Items</Table.Th>
+            <Table.Th>Stock</Table.Th>
+            <Table.Th>Action</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+      <Space h="md" />
+      <Flex
+        mih={50}
+        gap="md"
+        justify="center"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+        <Pagination
+          total={totalCount}
+          value={activePage}
+          onChange={setActivePage}
+          mt="sm"
+        />
+      </Flex>
+      <Space h="md" />
+    </div>
+  );
+}
