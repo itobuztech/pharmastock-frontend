2f5f220657ac3dfaf1cd1b79732145edd9de0d0c
Kunal Biswas <kunal@itobuz.com>
2024-07-17 13:31:43 +0530

    feat: IOM-82 organization code refactor done

---
+++ client/src/Page/Organizations/components/OrganizationTable.tsx
@@ -0,0 +1,81 @@
+import { Flex, Pagination, Space, Table } from "@mantine/core";
+import { OrganizationList } from "interfaces/interfaces";
+import React from "react";
+import ActionPopover from "Components/ActionPopover";
+import { useNavigate } from "react-router-dom";
+import routes from "Lib/Routes/Routes";
+
+export default function OrganizationTable({
+  activePage,
+  setActivePage,
+  organizationList,
+  handleDelete,
+  totalCount,
+}: {
+  activePage: number;
+  setActivePage: React.Dispatch<React.SetStateAction<number>>;
+  organizationList?: OrganizationList["organizations"];
+  handleDelete: (id: string) => void;
+  totalCount: number;
+}) {
+  const navigate = useNavigate();
+
+  function screenSwitch(id: string) {
+    navigate(`${routes.dashboard.organizations.path}/${id}`);
+  }
+
+  const rows = organizationList?.organizations?.map((org, i) => (
+    <Table.Tr key={org.id}>
+      <Table.Td>
+        {activePage === 1 ? i + 1 : (activePage - 1) * 10 + (i + 1)}
+      </Table.Td>
+      <Table.Td>{org.name}</Table.Td>
+      <Table.Td className="w-2/5">{org.description}</Table.Td>
+      <Table.Td>{org.city}</Table.Td>
+      <Table.Td>{org.address}</Table.Td>
+      <Table.Td>
+        <ActionPopover
+          handleView={() => screenSwitch(org.id)}
+          handleDelete={() => handleDelete(org.id)}
+        />
+      </Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <div className=" bg-white">
+      <Table horizontalSpacing="md" verticalSpacing="md">
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Sl No.</Table.Th>
+            <Table.Th>Name</Table.Th>
+            <Table.Th>Description</Table.Th>
+            <Table.Th>City</Table.Th>
+            <Table.Th>Address</Table.Th>
+            <Table.Th>Action</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+      <Space h="md" />
+      <Flex
+        mih={50}
+        gap="md"
+        justify="center"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+        {
+          <Pagination
+            total={totalCount}
+            value={activePage}
+            onChange={setActivePage}
+            mt="sm"
+          />
+        }
+      </Flex>
+      <Space h="md" />
+    </div>
+  );
+}
