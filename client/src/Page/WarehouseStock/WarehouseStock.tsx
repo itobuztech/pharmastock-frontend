066768cdf8b3cbe9795fa3ec103ef90e3727015c
Kaustavi <kaustavi@itobuz.com>
2024-07-18 16:53:45 +0530

    feat: form close added in stock form IOM-91

--- client/src/Page/WarehouseStock/WarehouseStock.tsx
+++ client/src/Page/WarehouseStock/WarehouseStock.tsx
@@ -85,6 +85,7 @@ export default function WarehouseStock() {
         <WarehouseStockForm
           selectOrgItem={selectOrgItem}
           selectWarehouseItem={selectWarehouseItem}
+          close={close}
         />
       </Modal>
     </section>
