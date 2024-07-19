cb1188dea1d7e26f8136fea120d70a8b73f548dd
Kunal Biswas <kunal@itobuz.com>
2024-07-19 14:33:49 +0530

    feat: IOM-95 user list done

---
+++ client/src/query/user/usersList.ts
@@ -0,0 +1,23 @@
+import { gql } from "@apollo/client";
+
+export const GetUsersList = gql`
+  query Users {
+    users {
+      total
+      users {
+        createdAt
+        email
+        emailConfirmationToken
+        id
+        isEmailConfirmed
+        name
+        organization {
+          id
+          name
+        }
+        updatedAt
+        username
+      }
+    }
+  }
+`;
