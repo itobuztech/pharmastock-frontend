08e4d50935fe5c9f68d238b45fb2474388576a3e
Kunal Biswas <kunal@itobuz.com>
2024-07-23 15:18:22 +0530

    feat: IOM-117 pharmacy stock created done

--- client/src/Page/Pharmacy/PharmacyDetails.tsx
+++ client/src/Page/Pharmacy/PharmacyDetails.tsx
@@ -5,10 +5,14 @@ import { useQuery } from "@apollo/client";
 import { GetPharmacyDetails } from "query/pharmacy/pharmacyDetails";
 import { Pharmacy } from "gql/graphql";
 import PharmacyForm from "./components/PharmacyForm";
+import { Modal } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import PharmacyStockForm from "Page/PharmacyStock/components/PharmacyStockForm";
 
 export default function PharmacyDetails() {
   const [editForm, setEditForm] = useState(false);
   const { id } = useParams();
+  const [opened, { open, close }] = useDisclosure(false);
 
   const { data: pharmacyDetails, refetch } = useQuery<{ pharmacy: Pharmacy }>(
     GetPharmacyDetails,
@@ -24,7 +28,8 @@ export default function PharmacyDetails() {
       <PageHeader
         title="Pharmacy Details"
         showBackButton={true}
-        showCreateButton={false}
+        showCreateButton={true}
+        onClick={open}
       />
 
       <div className="w-full lg:w-1/2 bg-white rounded-md py-6 px-6">
@@ -37,6 +42,20 @@ export default function PharmacyDetails() {
           setEditForm={setEditForm}
         />
       </div>
+
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Create Pharmacy Stock"
+        centered
+        size={"sm"}
+      >
+        <PharmacyStockForm
+          pharmacyName={pharmacyDetails?.pharmacy.name}
+          pharmacyId={pharmacyDetails?.pharmacy.id}
+          close={close}
+        />
+      </Modal>
     </section>
   );
 }
