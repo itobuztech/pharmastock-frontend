07be079e5cf1d1fc49868021b169e9e43e32c243
Kunal Biswas <kunal@itobuz.com>
2024-07-25 16:58:10 +0530

    feat: IOM-111 user details done

--- client/src/Page/Organizations/OrganizationsPage.tsx
+++ client/src/Page/Organizations/OrganizationsPage.tsx
@@ -166,6 +166,10 @@ export default function OrganizationsPage() {
         title="Create User"
         centered
         size={"lg"}
+        zIndex={600}
+        overlayProps={{
+          zIndex: 500,
+        }}
       >
         <UserCreateForm selectItem={selectItem} close={userModalClose} />
       </Modal>
