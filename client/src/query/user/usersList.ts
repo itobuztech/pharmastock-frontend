c20a35fd131e524a039bd74b164ab047b0ed0018
Kunal Biswas <kunal@itobuz.com>
2024-07-29 10:30:16 +0530

    feat: IOM-106 add search in user list

--- client/src/query/user/usersList.ts
+++ client/src/query/user/usersList.ts
@@ -1,8 +1,16 @@
 import { gql } from "@apollo/client";
 
 export const GetUsersList = gql`
-  query Users($paginationArgs: PaginationArgs) {
-    users(paginationArgs: $paginationArgs) {
+  query Users(
+    $pagination: Boolean
+    $paginationArgs: PaginationArgs
+    $searchText: String
+  ) {
+    users(
+      pagination: $pagination
+      paginationArgs: $paginationArgs
+      searchText: $searchText
+    ) {
       total
       users {
         createdAt
