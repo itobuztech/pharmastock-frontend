ad74edb68ba27eec0d68c05614a8401fc519b94d
Kaustavi Banerjee <kaustavi@itobuz.com>
2024-08-11 18:09:43 +0530

    fix: logo added on sign up and log in page

--- client/src/Page/Auth/Login/LoginPage.tsx
+++ client/src/Page/Auth/Login/LoginPage.tsx
@@ -82,8 +82,15 @@ export default function LoginPage() {
   return (
     <div
       style={{ height: `${height}px` }}
-      className="flex justify-center items-center"
+      className="flex flex-col justify-center items-center gap-7"
     >
+      <div
+          className="flex items-center justify-start mx-6 mt-10 no-underline"
+        >
+          <span className="text-black  ml-4 text-2xl font-bold">
+            Pharma Stock
+          </span>
+        </div>
       <div className="mx-auto flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow  sm:px-6 md:px-8 lg:px-10">
         <h1 className="self-center font-light text-black m-0">Welcome</h1>
         <h3 className="self-center font-light text-black m-0">
--- client/src/Page/Auth/Register/Register.tsx
+++ client/src/Page/Auth/Register/Register.tsx
@@ -72,10 +72,10 @@ export default function Register() {
     },
   });
 
-  const roleArray = Object.values(UserRole).map((role) => ({
-    value: role,
-    label: role,
-  }));
+  const roleArray = Object.entries(UserRole).map((role) => {return {
+    value: role[1],
+    label: role[0]
+  }});
 
   const [signUp, { loading: signUpLoading }] = useMutation(SIGNUP_MUTATION, {
     onError: (error) => {
@@ -94,8 +94,15 @@ export default function Register() {
   return (
     <div
       style={{ height: `${height}px` }}
-      className="flex justify-center items-center"
+      className="flex flex-col justify-center items-center gap-7"
     >
+        <div
+          className="flex items-center justify-start mx-6 mt-10 no-underline"
+        >
+          <span className="text-black  ml-4 text-2xl font-bold">
+            Pharma Stock
+          </span>
+        </div>
       <div className="m-auto flex flex-col w-full mx-4 md:w-[500px] md:mx-0 px-4 py-8 bg-white rounded-lg shadow  sm:px-6 md:px-6 lg:px-8">
         <div className="self-center mb-2 text-xl font-light text-gray-800 sm:text-2xl">
           Create a new account
