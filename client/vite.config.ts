a3ef77be73c502fff88a290739d462dd166f6de9
Kunal Biswas <kunal@itobuz.com>
2024-08-13 16:12:40 +0530

    fix: IOM-137 changes on vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import eslint from 'vite-plugin-eslint';
-import { VitePWA } from 'vite-plugin-pwa';
-import { reactScopedCssPlugin } from 'rollup-plugin-react-scoped-css'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import eslint from "vite-plugin-eslint";
+import { VitePWA } from "vite-plugin-pwa";
+import { reactScopedCssPlugin } from "rollup-plugin-react-scoped-css";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
-  base: '',
+  base: "/",
   plugins: [
     react(),
     viteTsconfigPaths(),
     eslint(),
     reactScopedCssPlugin(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       workbox: {
-        globPatterns: ['**/*.{tsx,ts,js,css,html,ico,png,svg}']
+        globPatterns: ["**/*.{tsx,ts,js,css,html,ico,png,svg}"],
       },
       devOptions: {
         enabled: false,
