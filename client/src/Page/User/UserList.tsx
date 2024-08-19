83282393e979bb77bfcc3a692ceecb0c1376d1b9
Kunal Biswas <kunal@itobuz.com>
2024-08-19 12:18:06 +0530

    fix: IOM-193 super admin delete users added

--- client/src/Page/User/UserList.tsx
+++ client/src/Page/User/UserList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import PageHeader from "Components/PageHeader";
-import { useLazyQuery } from "@apollo/client";
+import { useLazyQuery, useMutation } from "@apollo/client";
 import { GetUsersList } from "query/user/usersList";
 import { toast } from "react-toastify";
 import { ChildComponentProps, UserData, Users } from "interfaces/interfaces";
@@ -8,7 +8,14 @@ import UserTable from "./components/UserTable";
 import { LoadingOverlay } from "@mantine/core";
 import EmptyList from "Components/EmptyList";
 import Search from "Components/Search";
-import { useDebouncedCallback } from "@mantine/hooks";
+import { useDebouncedCallback, useDisclosure } from "@mantine/hooks";
+import ConfirmationModal from "Components/ConfirmationModal";
+import { useAppSelector } from "Lib/Store/hooks";
+import {
+  USER_PERMISSION_CAPABILITIES,
+  USER_PERMISSION_FIELDS,
+} from "enums/enums";
+import { DeleteUserBySuperAdmin } from "query/user/userDelete";
 
 export default function UserList({
   handleUserPermissions,
@@ -17,6 +24,12 @@ export default function UserList({
   const [activePage, setActivePage] = useState(1);
   const [totalCount, setTotalCount] = useState(1);
   const [searchInput, setSearchInput] = useState("");
+  const [deletedId, setDeletedId] = useState<string>();
+  const [
+    deleteModalOpened,
+    { open: deleteModalOpen, close: deleteModalClose },
+  ] = useDisclosure(false);
+  const permission = useAppSelector((state) => state.user.permission);
 
   /* ====== User List Query ====== */
   const [fetchUserList, { refetch, loading }] = useLazyQuery<UserData>(
@@ -71,6 +84,41 @@ export default function UserList({
     handleSearch(event.currentTarget.value);
   };
 
+  /* ====== User Delete Query ====== */
+  const [deleteUser] = useMutation(DeleteUserBySuperAdmin, {
+    onError: (err) => {
+      toast.error(err.message);
+    },
+    onCompleted: () => {
+      refetch().then(({ data }) => {
+        if (data) {
+          const users = data.users;
+          const total = data.users.total;
+          const paginationCount = Math.ceil(total / 10);
+
+          setUserList(users);
+          setTotalCount(paginationCount);
+        }
+      });
+      deleteModalClose();
+      toast.success("User Deleted Successfully");
+    },
+  });
+
+  /* ====== Handle Delete Function ====== */
+  function handleDelete(itemId: string) {
+    const deleteItem = userList?.users.find((x) => x.id === itemId);
+    setDeletedId(deleteItem?.id);
+    console.log(deleteItem?.id);
+    deleteModalOpen();
+  }
+
+  function getDeleteUser() {
+    deleteUser({
+      variables: { deleteUserInput: { id: deletedId } },
+    });
+  }
+
   return (
     <section className="min-h-screen bg-blue-50 bg-opacity-50 py-4 md:py-8 px-4 md:px-8">
       <PageHeader title="Users" showCreateButton={false} />
@@ -100,9 +148,23 @@ export default function UserList({
           setActivePage={setActivePage}
           totalCount={totalCount}
           userList={userList}
+          handleDelete={handleDelete}
           handleUserPermissions={handleUserPermissions}
+          showDeleteButton={handleUserPermissions(
+            permission,
+            USER_PERMISSION_FIELDS.USER_PERMISSION,
+            USER_PERMISSION_CAPABILITIES.DELETE
+          )}
         />
       )}
+
+      {/* ==== Delete Confirmation Modal ==== */}
+      <ConfirmationModal
+        title="User"
+        modalOpen={deleteModalOpened}
+        modalClose={deleteModalClose}
+        deleteItem={() => getDeleteUser()}
+      />
     </section>
   );
 }
