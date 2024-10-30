4a7911c3a74dbb2c92c236706d375bcff44eb541
Rimpa Das <rimpa@itobuz.com>
2024-10-30 13:11:27 +0530

    feat: stocks history responsive issue fixed IOM-238

--- client/src/Page/Dashboard/StocksHistory/Components/StocksHistoryDetailsCard.tsx
+++ client/src/Page/Dashboard/StocksHistory/Components/StocksHistoryDetailsCard.tsx
@@ -7,7 +7,6 @@ import {
   Text,
   Badge,
   Group,
-  Grid,
 } from "@mantine/core";
 import { format, parseISO } from "date-fns";
 
@@ -39,7 +38,7 @@ export default function StocksHistoryDetailsCards({
   const cards =
     stocksMovementList &&
     stocksMovementList.stockMovementsByLotName.map((item) => (
-      <Grid.Col span={4} key={item.id}>
+      <div className="p-2" key={item.id}>
         <Card shadow="sm" padding="lg" radius="md" withBorder>
           <Group className="flex justify-between items-center mb-4">
             <Text className="text-xl font-bold capitalize text-gray-800">
@@ -56,76 +55,78 @@ export default function StocksHistoryDetailsCards({
             </Badge>
           </Group>
 
-          <Grid columns={2} className="text-gray-700">
-            <Grid.Col span={1} className="p-2 border-b">
+          <div className="grid grid-cols-2 text-gray-700">
+            <div className="p-2 border-b">
               <Text className="font-bold text-gray-600">Created On</Text>
               <Text className="font-medium text-gray-800">
                 {format(parseISO(item.createdAt), "MM/dd/yyyy")}
               </Text>
-            </Grid.Col>
-            <Grid.Col span={1} className="p-2 border-b">
+            </div>
+            <div className="p-2 border-b">
               <Text className="font-bold text-gray-600">Updated On</Text>
               <Text className="font-medium text-gray-800">
                 {format(parseISO(item.updatedAt), "MM/dd/yyyy")}
               </Text>
-            </Grid.Col>
-            <Grid.Col span={1} className="p-2">
+            </div>
+            <div className="p-2">
               <Text className="font-bold text-gray-600">Lot Name</Text>
               <Text className="font-medium text-gray-800">{item.lotName}</Text>
-            </Grid.Col>
+            </div>
 
-            <Grid.Col span={1} className="p-2 border-b">
+            <div className="p-2 border-b">
               <Text className="font-bold text-gray-600">Product</Text>
               <Text className="font-medium text-gray-800">{item.item}</Text>
-            </Grid.Col>
-            <Grid.Col span={1} className="p-2 border-b">
+            </div>
+            <div className="p-2 border-b">
               <Text className="font-bold text-gray-600">Organisation</Text>
               <Text className="font-medium text-gray-800">
                 {item.organisation}
               </Text>
-            </Grid.Col>
+            </div>
 
-            <Grid.Col span={1} className="p-2 border-b">
+            <div className="p-2 border-b">
               <Text className="font-bold text-gray-600">Warehouse</Text>
               <Text className="font-medium text-gray-800">
                 {item.warehouse ?? "N/A"}
               </Text>
-            </Grid.Col>
-            <Grid.Col span={1} className="p-2 border-b">
+            </div>
+            <div className="p-2 border-b">
               <Text className="font-bold text-gray-600">Pharmacy</Text>
               <Text className="font-medium text-gray-800">
                 {item.pharmacy ?? "N/A"}
               </Text>
-            </Grid.Col>
+            </div>
 
-            <Grid.Col span={1} className="p-2 border-b">
+            <div className="p-2 border-b">
               <Text className="font-bold text-gray-600">
                 Pharmacy Clearance
               </Text>
               <Text className="font-medium text-gray-800">
                 {item.pharmacyClearance ?? "N/A"}
               </Text>
-            </Grid.Col>
-            <Grid.Col span={1} className="p-2">
+            </div>
+            <div className="p-2">
               <Text className="font-bold text-gray-600">Expiry Date</Text>
               <Text className="font-medium text-gray-800">
                 {item.expiry
                   ? format(parseISO(String(item.expiry)), "MM/dd/yyyy")
                   : "N/A"}
               </Text>
-            </Grid.Col>
-            <Grid.Col span={1} className="p-2 border-b">
+            </div>
+            <div className="p-2 border-b">
               <Text className="font-bold text-gray-600">Qty</Text>
               <Text className="font-medium text-gray-800">{item.qty}</Text>
-            </Grid.Col>
-          </Grid>
+            </div>
+          </div>
         </Card>
-      </Grid.Col>
+      </div>
     ));
 
   return (
     <div className="bg-white overflow-auto p-4">
-      <Grid gutter="lg">{cards && cards.length > 0 && cards}</Grid>
+      <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3">
+        {cards && cards.length > 0 && cards}
+      </div>
       <Space h="md" />
       <Flex
         mih={50}
