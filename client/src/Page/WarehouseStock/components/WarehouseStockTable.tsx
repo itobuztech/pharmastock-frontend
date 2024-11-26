0627f9d3119133dc882ef49d5866bb982defd4a7
Rimpa Das <rimpa@itobuz.com>
2024-11-26 11:48:59 +0530

    feat: comma seperator added in number IOM-267

--- client/src/Page/WarehouseStock/components/WarehouseStockTable.tsx
+++ client/src/Page/WarehouseStock/components/WarehouseStockTable.tsx
@@ -7,6 +7,7 @@ import {
 } from "enums/enums";
 import { Permissions, WarehouseStocks } from "interfaces/interfaces";
 import routes from "Lib/Routes/Routes";
+import { formatPriceWithComma } from "Page/Product/ProductList";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -43,12 +44,12 @@ export default function WarehouseStockTable({
       <Table.Td>{format(parseISO(item.createdAt), "MM/dd/yyyy")}</Table.Td>
       <Table.Td>{item.item.name}</Table.Td>
       <Table.Td>{item.warehouse.name}</Table.Td>
-      <Table.Td>{item.finalQty}</Table.Td>
+      <Table.Td>{formatPriceWithComma(item.finalQty)}</Table.Td>
       <Table.Td>
-        {item.currency} {item.totalWholesalePrice}
+        {item.currency} {formatPriceWithComma(item.totalWholesalePrice as number)}
       </Table.Td>
       <Table.Td>
-        {item.currency} {item.totalMrpBaseUnit}
+        {item.currency} {formatPriceWithComma(item.totalMrpBaseUnit as number)}
       </Table.Td>
       <Table.Td>{item.SKU.sku}</Table.Td>
       <Table.Td className="text-right">
