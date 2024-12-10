693b60957f17c03c19769f7ee9a108701183b99e
Kaustavi <kaustavi@itobuz.com>
2024-12-10 12:49:41 +0530

    fix: duplicate code of dev

--- client/src/aplloClient.ts
+++ client/src/aplloClient.ts
@@ -19,7 +19,7 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-const client = new ApolloClient({
+const client = new ApolloClient({   
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   defaultOptions: {
