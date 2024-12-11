01df15342ee4aa94588a638afba3f8c5ae0ce322
Kaustavi <kaustavi@itobuz.com>
2024-12-11 15:34:44 +0530

    fix: set api_url after login

--- client/src/Page/Auth/Login/LoginPage.tsx
+++ client/src/Page/Auth/Login/LoginPage.tsx
@@ -56,6 +56,7 @@ export default function LoginPage() {
         if (d) {
           dispatch(setUser(d.login.user));
           localStorage.setItem(appConfig.storage.userData, JSON.stringify(d.login));
+          localStorage.setItem(appConfig.storage.apiURL, appConfig.api.graphql);
           navigate(`${routes.dashboard.profile.path}`);
           toast.success(messagesData.login.successMessage);
         }
